c4deb6648f560ecf950748d34a9ea423f6cb770d
Nambi-Nachiyar <suhamuki@gmail.com>
2025-06-22 20:50:10 +0530

    css-change
    css-change

--- GUVI-Project-task-3/medium-Level/medium.js
+++ GUVI-Project-task-3/medium-Level/medium.js
@@ -21,7 +21,8 @@ let guessList = [
 const keys = [
     ["Q","W","E","R","T","Y","U","I","O","P"],
     ["A","S","D","F","G","H","J","K","L"],
-    ["Enter","Z","X","C","V","B","N","M","Back"]
+    ["Z","X","C","V","B","N","M"],
+    ["Enter","Back"]
 ]
 console.log(keys.length)
 let guess = wordList.concat(guessList)
